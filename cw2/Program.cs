3be58b3cb0b4bc23583403a7318fe08424d99dc6
s19911 <s19911@LAB003-09>
2020-03-09 11:42:59 +0100

    created cw2

---
+++ cw2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace cw2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string file = @"data\dane.csv";
+
+            FileInfo f = new FileInfo(file);
+            using (StreamReader stream = new StreamReader(f.OpenRead()))
+            {
+                string line = " ";
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            //stream.Dispose();
+        }
+    }
+}
